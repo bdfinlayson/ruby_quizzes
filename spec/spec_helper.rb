501f74ba4130e58d00ca7cef04fe2ec478cb68eb
Bryan Finlayson <bdfinlayson@gmail.com>
2015-07-06 18:37:11 -0500

    Add directory structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
-require_relative '../quizzes/fizzbuzz'
-require_relative '../quizzes/solitaire_cipher'
+require_relative '../quizzes/fizzbuzz/fizzbuzz'
+require_relative '../quizzes/solitaire_cipher/solitaire_cipher'
