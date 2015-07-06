0eb6ed5793cb802cd4cd7adf9f532ddd9068cb4f
Bryan Finlayson <bdfinlayson@gmail.com>
2015-07-06 13:19:44 -0500

    Add solution to FizzBuzz with spec tests

--- quizzes/fizzbuzz_126.rb
+++ quizzes/fizzbuzz_126.rb
@@ -1,5 +1,45 @@
+# http://rubyquiz.com/quiz126.html
+
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the
+# number and for the multiples of five print “Buzz”. For
+# numbers which are multiples of both three and five
+# print “FizzBuzz”.
+
+
 class FizzBuzz
-  def hello
-    'Hello World'
+
+  def compile_result(iterations)
+    array = Array.new
+
+    iterations.times do |num|
+      if is_multiple_of_both?(num.next)
+        array << "FizzBuzz"
+      elsif is_multiple_of_3?(num.next)
+        array << "Fizz"
+      elsif is_multiple_of_5?(num.next)
+        array << "Buzz"
+      else
+        array << num.next
+      end
+    end
+
+    print_fizzbuzz(array)
+  end
+
+  def print_fizzbuzz(array)
+    array.each { |item| puts item }
+  end
+
+  def is_multiple_of_3?(num)
+    num % 3 == 0 ? true : false
+  end
+
+  def is_multiple_of_5?(num)
+    num % 5 == 0 ? true : false
+  end
+
+  def is_multiple_of_both?(num)
+    true if is_multiple_of_3?(num) && is_multiple_of_5?(num)
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,47 @@
 require 'spec_helper'
 
-describe FizzBuzz, '#hello' do
-  context 'says hello world' do
-    it 'should say hello world' do
-      fizzbuzz = FizzBuzz.new
-      expect(fizzbuzz.hello).to eq 'Hello World'
+describe FizzBuzz, '#is_multiple_of_3?' do
+  context 'should find a multiple of 3' do
+    it 'should find the multiple of 3 of a single number' do
+      fb = FizzBuzz.new
+      expect(fb.is_multiple_of_3?(9)).to eq true
+    end
+
+    it 'should return false for numbers not multiples of 3' do
+      fb = FizzBuzz.new
+      expect(fb.is_multiple_of_3?(10)).to eq false
+    end
+  end
+end
+
+describe FizzBuzz, '#is_multiple_of_5?' do
+  context 'should find a multiple of 5' do
+    it 'should find the multiple of 5 of a single number' do
+      fb = FizzBuzz.new
+      expect(fb.is_multiple_of_5?(10)).to eq true
+    end
+
+    it 'should return false for numbers not multiples of 5' do
+      fb = FizzBuzz.new
+      expect(fb.is_multiple_of_5?(7)).to eq false
+    end
+  end
+end
+
+describe FizzBuzz, '#is_multiple_of_both?' do
+  context 'should determine if number is multiple of 3 and 5' do
+    it 'should return true if conditions are met' do
+      fb = FizzBuzz.new
+      expect(fb.is_multiple_of_both?(15)).to eq true
+    end
+  end
+end
+
+describe FizzBuzz, '#compile_result' do
+  context 'should iterate x times to find the result' do
+    it 'should return an array with the correct result' do
+      fb = FizzBuzz.new
+      expect(fb.compile_result(15)).to eq [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz']
     end
   end
 end
